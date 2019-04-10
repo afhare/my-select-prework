5b5cdeff78e1f4e580edd51522d8eba9064952e1
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-10 13:16:54 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ i=0 
+ new_collection = []
+ while i< collection.length
+  if yield collection[i]
+    new_collection << collection[i]
+    i+=1 
+  end
+ end
 end
